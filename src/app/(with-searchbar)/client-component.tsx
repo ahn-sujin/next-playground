9282e9742514e59d34284622c8335ba35b9e8df6
ahn-sujin <asj9674@gmail.com>
2025-02-11 19:42:26 +0900

    add:서버 컴포넌트

---
+++ src/app/(with-searchbar)/client-component.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+// import ServerComponent from "./server-component";
+
+export default function ClientComponent() {
+  console.log("클라이언트");
+
+  return (
+    <div>
+      클라이언트 컴포넌트
+      {/* <ServerComponent /> */}
+    </div>
+  );
+}
