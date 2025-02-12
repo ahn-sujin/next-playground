21fea776d835ad83770a866ad5ab68db72d3d97d
ahn-sujin <asj9674@gmail.com>
2025-02-12 20:19:14 +0900

    add:데이터 페칭

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,16 +1,29 @@
 import BookItem from "@/components/book-item";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{
     q?: string;
   }>;
 }) {
+  // 검색 도서
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${
+      (
+        await searchParams
+      ).q
+    }`
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다</div>;
+  }
+  const searchBooks: BookData[] = await response.json();
+
   return (
     <div>
-      {books.map((book) => (
+      {searchBooks.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
     </div>
