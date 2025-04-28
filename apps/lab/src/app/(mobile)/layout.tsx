5094b5bf3c199dfafb78fa58faf6a03342af0514
ahn-sujin <asj9674@gmail.com>
2025-04-28 23:02:03 +0900

    add:디렉토리 정리

---
+++ apps/lab/src/app/(mobile)/layout.tsx
@@ -0,0 +1,31 @@
+import Header from "@/components/layouts/Header";
+import NavigationBar from "@/components/layouts/NavigationBar";
+import Link from "next/link";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header
+        left={
+          <>
+            <Link href="/">홈</Link>
+          </>
+        }
+        center="titletitletitletitletitletitletitletitle"
+        right={
+          <>
+            <Link href="/">마이</Link>
+            <Link href="/">마이</Link>
+            <Link href="/">마이</Link>
+          </>
+        }
+      />
+      {children}
+      <NavigationBar />
+    </>
+  );
+}
---
+++ apps/lab/src/app/oom/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Welcome to the OOM Page</h1>
+      <p>This is a basic Next.js page.</p>
+    </div>
+  );
+};
+
+export default Page;
